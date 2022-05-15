5d0d072a356cac129075e1722dfe9f385e67a284
Santanu55555 <99542723+Santanu55555@users.noreply.github.com>
2022-05-15 15:59:00 +0530

    Dino game

---
+++ chrome-game/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ChromeDinoGame from 'react-chrome-dino';
+
+
+function App() {
+  return (
+    <div className="App">
+        <ChromeDinoGame />
+    </div>
+  );
+}
+
+export default App;
